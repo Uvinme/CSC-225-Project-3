c2e1ebc47ab6a603e1368e538f160d02513623a3
Uvinme <uvinme@gmail.com>
2020-05-24 11:11:42 -0400

    First commit

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
 jQuery(document).ready(function ($){
-    function createBookListItem(Book) {
+    //Loading Notification
+    $('body').append('<div style="" id="loadingDiv"><div class="loader">Loading...</div></div>');
+    $(window).on('load', function(){
+    setTimeout(removeLoader, 2000); //wait for page load 
+    });
+    function removeLoader(){
+    $( "#loadingDiv" ).fadeOut(500, function() {
+      // fadeOut complete. Remove the loading div
+      $( "#loadingDiv" ).remove(); //makes page more lightweight 
+    });  
+    }
+    function createBookListItem(Book) {       
         var $li = $('<li>'); //<li></li>
         $li.addClass('list-group-item hover-invert cursor-pointer'); //<li class="list-group-item"></li>
         $li.html(Book.title); //<li class="list-group-item">{TEXT}</li>
         $li.data('BookId', Book.id);
         return $li;
     } 
-
-    $('#Book-list').html('Loading ...');
     var request = axios.get('http://csc225.mockable.io/books');
     request.then(function (response){
         response.data.forEach(function (Book) {
             $('#Book-list').prepend(createBookListItem(Book));
-            
-        });
-        
+        });     
         $('.list-group-item').on('click', function(){
             $('.list-group-item').removeClass('active');
             var BookId = $(this).data('BookId');
@@ -22,16 +29,25 @@ jQuery(document).ready(function ($){
             $('#cover').html('Loading ...');
             axios.get('http://csc225.mockable.io/books/' + BookId).then(
                 function(response){
-                    console.log(response.data.cover);
                     var $img = $('<img>').attr('src', response.data.cover).attr('alt', response.data.title);
                     $('#cover').html($img);
-
+                    var $author = ('By ' + response.data.author);
+                    $('#author').html($author);
+                    var $country = ('Country - ' + response.data.country);
+                    $('#country').html($country);        
+                    var $language = ('Language - ' + response.data.language);
+                    $('#language').html($language);                               
+                    var url = response.data.link;                
+                    document.getElementById("link").setAttribute("href",url);
+                    console.log(url);
+                    var $pages = ('No of Pages - ' + response.data.pages);
+                    $('#pages').html($pages);
+                    var $title = response.data.title;
+                    $('#title').html($title);  
+                    var $year = ('Year - ' + response.data.year);
+                    $('#year').html($year);                                                                                    
                 }
-            );
-            
-        });
-        
-    });
-    console.log('hello!');
-
+            );           
+        });       
+    });       
 }); 
